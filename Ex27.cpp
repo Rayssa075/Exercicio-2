686eda08658ca467eb81b80e9578726010b6defc
Rayssa075 <andressarayssa05@hotmail.com>
2026-03-31 23:17:42 -0300

    Update Ex27.cpp

---
+++ Ex27.cpp
@@ -0,0 +1,36 @@
+#include <ioestream>
+using namespace std;    
+int main () {
+    
+    int mês = 0;
+    cout<< "Digite um numero de 1 a 12 para saber o mês correspondente: ";
+    cin >> mês;
+
+    switch (mês) {
+        case 1;
+        case 2;
+        case 3;
+        cout<< "1 trimestre do ano (Janeiro, Fevereiro e Março)"<< endl;
+        break;
+        case 4;
+        case 5;
+        case 6;
+        cout<< "2 trimestre do ano (Abril, Maio e Junho )"<< endl;
+        break;
+        case 7;
+        case 5;
+        case 9;
+         cout<< " 3 trimestre do ano (Julho, Agosto e Setemvbro)" << endl;
+         vreak.
+         case 10;
+         case 11;
+         case 12;
+         cout<< "4 trimestre do ano (Outubro, Novembro e Dezembro)" << endl;
+         break;
+         default;
+         cout<< "ERROS!!! este numero é invalido, por favor digite um numero de 1 a 12" <, endl;
+         break;
+        }
+
+        return 0;
+}   
